57a22f4416d48390743afdc37c64f5ee8fd9780d
사용자이름 <>
2026-03-22 05:15:18 +0900

    fix: 마이크 아이콘 파일명 변경(Cache Busting) 및 원격 빌드

--- lib/widgets/simplified_input_widget.dart
+++ lib/widgets/simplified_input_widget.dart
@@ -649,7 +649,7 @@ class MeshMicIcon extends StatelessWidget {
       ),
       child: Center(
         child: Image.asset(
-          'assets/icon_mic.png',
+          'assets/icon_mic_yellow.png',
           width: size * 0.9,
           height: size * 0.9,
           fit: BoxFit.contain,
